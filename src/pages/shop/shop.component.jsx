be4bd39335ca11160abeda13829c19a96b7da097
Yash Mody <yashmody@buffalo.edu>
2020-05-15 09:19:12 -0500

    added a shop page, with collection-preview and collection-item components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+import './shop.styles.scss';
+import SHOP_DATA from './shop.data';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render() {
+        const {collections} = this.state;
+        return (<div className="shop-page">
+            {
+                collections.map(({id, ...othercollectionProps}) => (
+                    <CollectionPreview key={id} {...othercollectionProps}/>
+                ))
+            }
+        </div>);
+    }
+}
+
+export default ShopPage;
